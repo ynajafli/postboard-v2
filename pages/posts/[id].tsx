0baf7ec0a0ca06b58498a3dc035617992323a982
Yusif Najafli <ynajafli@outlook.com>
2026-05-21 10:00:31 -0600

    complete step 2

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,11 +1,15 @@
-
+import { useRouter } from "next/router";
 
 export default function Posts() {
+    const router = useRouter();
+    const { id } = router.query;
 
+    if (typeof id !== 'string') return <p>Loading...</p>
 
     return(
         <>
             <h1>post</h1>
+            <p>id: {id}</p>
         </>
     );
 }
