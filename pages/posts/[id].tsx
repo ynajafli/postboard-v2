6b90dd758d16576b3fc04cb2854f242d441e45f8
Yusif Najafli <ynajafli@outlook.com>
2026-05-26 09:07:58 -0600

    finish step 4

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,15 +1,21 @@
-import { useRouter } from "next/router";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { Post, User } from "@/types";
+import { useFetch } from "@/hooks/useFetch";
 
-export default function Posts() {
-    const router = useRouter();
-    const { id } = router.query;
-
-    if (typeof id !== 'string') return <p>Loading...</p>
+export default function Posts({ post }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    
 
     return(
         <>
-            <h1>post</h1>
-            <p>id: {id}</p>
+            <h1 className="text-4xl font-bold">{post.title}</h1>
         </>
     );
+}
+
+export const getServerSideProps: GetServerSideProps<{ post: Post}> = async (context) => {
+    const { id } = context.params!;
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+    const post = await res.json();
+    return { props: { post } }
 }
