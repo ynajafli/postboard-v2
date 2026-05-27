bd6b0bea063b2eb29497b4a44ecb3062c30cd7c9
Yusif Najafli <ynajafli@outlook.com>
2026-05-27 09:52:00 -0600

    Finish step 5

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,21 +1,41 @@
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import { Post, User } from "@/types";
-import { useFetch } from "@/hooks/useFetch";
+import { Comment, Post, User } from "@/types";
 
-export default function Posts({ post }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Posts({ post, comments, user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     
 
     return(
         <>
             <h1 className="text-4xl font-bold">{post.title}</h1>
+            <p>By {user.name}</p>
+            <p className="mt-5">{post.body}</p>
+            <h2 className="text-3xl font-bold mt-5">Comments</h2>
+            <ul>
+                {comments.map(comment => (
+                    <li key={comment.id}>{comment.body}</li>
+                ))}
+            </ul>
         </>
     );
 }
 
-export const getServerSideProps: GetServerSideProps<{ post: Post}> = async (context) => {
-    const { id } = context.params!;
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+export const getServerSideProps: GetServerSideProps<{ post: Post, comments: Comment[], user: User }> = async (context) => {
+    try {
+        const { id } = context.params!;
 
-    const post = await res.json();
-    return { props: { post } }
+        const postRes = fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        const commentRes = fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+
+        const [post, comments] = await Promise.all([
+            postRes.then(res => res.json()),
+            commentRes.then(res => res.json()),
+        ])
+
+        const userRes = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        const user = await userRes.json();
+
+        return { props: { post, comments, user } }
+    } catch (err) {
+        return { notFound: true}
+    }
 }
