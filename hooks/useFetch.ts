e9ccc17b1c5ad93f60be040d93c5e91fde28b817
Yusif Najafli <ynajafli@outlook.com>
2026-05-28 09:02:58 -0600

    finish step 7

--- hooks/useFetch.ts
+++ hooks/useFetch.ts
@@ -8,13 +8,14 @@ export function useFetch<T>(url: string): { data: T | null, isLoading: boolean, 
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-
-
+        const controller = new AbortController();
         async function fetchData() {
 
             try {
                 setIsLoading(true);
-                const response = await fetch(url);
+                const response = await fetch(url, {
+                    signal: controller.signal
+                });
 
                 if (response.ok) {
                     const data = await response.json();
@@ -23,6 +24,7 @@ export function useFetch<T>(url: string): { data: T | null, isLoading: boolean, 
                     throw new Error(`${response.statusText} Error: ${response.status}`);
                 }
             } catch (error) {
+                if (error instanceof Error && error.name === 'AbortError') return;
                 setError(error instanceof Error ? error.message : "Error");
             } finally {
                 setIsLoading(false);
@@ -30,9 +32,11 @@ export function useFetch<T>(url: string): { data: T | null, isLoading: boolean, 
         }
 
         fetchData();
+
+        return () => {
+            controller.abort();
+        }
     }, [url])
 
     return {data, isLoading, error}
-
-
 }
